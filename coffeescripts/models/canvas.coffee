550e79447ebc2dc748ad3a18cd1853b5658103b3
j.taneichi <j.tane3ma@gmail.com>
2015-01-09 13:22:23 +0900

    create CanvasModel

---
+++ coffeescripts/models/canvas.coffee
@@ -0,0 +1,4 @@
+define ['jquery', 'backbone'], ($, Backbone) ->
+  class Canvas extends Backbone.Model
+    defaults:
+      name: 'Canvas.Model'
---
+++ js/lib/models/canvas.js
@@ -0,0 +1,20 @@
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+define(['jquery', 'backbone'], function($, Backbone) {
+  var Canvas;
+  return Canvas = (function(_super) {
+    __extends(Canvas, _super);
+
+    function Canvas() {
+      return Canvas.__super__.constructor.apply(this, arguments);
+    }
+
+    Canvas.prototype.defaults = {
+      name: 'Canvas.Model'
+    };
+
+    return Canvas;
+
+  })(Backbone.Model);
+});
