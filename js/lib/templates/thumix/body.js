d9b681cb64c09493140775af03a75d5ab2e6a686
j.taneichi <j.tane3ma@gmail.com>
2015-01-13 18:42:50 +0900

    ver 0.0.1

--- js/lib/templates/thumix/body.js
+++ js/lib/templates/thumix/body.js
@@ -5,7 +5,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<div table-cell=\"middle\"><div data-js=\"canvases\" module=\"canvases\"></div><div data-js=\"tool\"></div></div>");;return buf.join("");
+buf.push("<div table-cell=\"middle\"><h4 project-Title=\"project-Title\">Thumix<div version=\"version\">version 0.0.1</div></h4><div data-js=\"canvases\" module=\"canvases\"></div><div data-js=\"tool\"></div></div>");;return buf.join("");
 };
 
 });
